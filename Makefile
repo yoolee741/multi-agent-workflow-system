1491e25c638a56feec370985fc009b72f0d6b4c1
Yoo Rim Lee <yoolee741@gmail.com>
2025-08-04 22:32:26 +0900

    [ADD] docker

---
+++ Makefile
@@ -0,0 +1,26 @@
+# Makefile
+include: .env
+.PHONY: help check-docker local-run clean rebuild
+
+help: ## Make 설명
+	@IFS=$$'\n' ; \
+	help_lines=(`fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//'`); \
+	for help_line in $${help_lines[@]}; do \
+		IFS=$$'#' ; \
+		help_split=($$help_line) ; \
+		help_command=`echo $${help_split[0]} | sed -e 's/^ *//' -e 's/ *$$//'` ; \
+		help_info=`echo $${help_split[2]} | sed -e 's/^ *//' -e 's/ *$$//'` ; \
+		printf "%-30s %s\n" $$help_command $$help_info ; \
+	done
+
+check-docker: ## 도커 체크
+	docker --version && docker-compose --version
+
+local-run: ## 로컬 서버 실행
+	docker-compose --env-file .env up -d && docker-compose logs -f
+
+clean: ## 도커 종료
+	docker-compose down
+
+rebuild: ## 도커 재빌드
+	docker-compose build
--- init.sql
+++ init.sql
@@ -1,17 +1,17 @@
 SET TIME ZONE 'UTC';
 
-create table if not exists user
+create table if not exists users
 (
     user_id serial primary key,
     created_at timestamptz not null default current_timestamp,
     name varchar(255) not null unique,
     auth_token varchar(255) not null
 );
-comment on table user is '유저 테이블';
-comment on column user.user_id is '유저 고유 ID';
-comment on column user.created_at is '생성일시';
-comment on column user.name is '유저 - 과제용 임시 아이디, 비밀번호 X';
-comment on column user.auth_token is 'jwt 토큰 - 권한 확인용 토큰 (유효기간 만료 시 재발급 로직은 반영X)';
+comment on table users is '유저 테이블';
+comment on column users.user_id is '유저 고유 ID';
+comment on column users.created_at is '생성일시';
+comment on column users.name is '유저 - 과제용 임시 아이디, 비밀번호 X';
+comment on column users.auth_token is 'jwt 토큰 - 권한 확인용 토큰 (유효기간 만료 시 재발급 로직은 반영X)';
 
 -- 워크플로우 상태 Enum 타입 생성
 do $$
@@ -27,14 +27,14 @@ create table if not exists workflow
     workflow_id UUID primary key,
     created_at timestamptz not null default current_timestamp,
     model varchar(255) not null default 'openai/gpt-4o-mini-2024-07-18',
-    user_id integer references user (user_id) on delete cascade, 
+    user_id integer references users (user_id) on delete cascade, 
     status status_enum not null default 'pending'
 );
 comment on table workflow is 'workflow 테이블';
 comment on column workflow.workflow_id is '워크플로우 고유 ID';
 comment on column workflow.created_at is '생성 일시';
 comment on column workflow.model is '사용된 모델 이름 (예: openai/gpt-4o)';
-comment on column workflow.user_id is 'user 테이블의 외래키 - 해당 에이전트를 생성한 유저 ID';
+comment on column workflow.user_id is 'users 테이블의 외래키 - 해당 에이전트를 생성한 유저 ID';
 comment on column workflow.status is 'workflow의 상태 - `pending`, `running`, `completed`, `failed`';
 
 create table if not exists data_collector
@@ -112,3 +112,11 @@ comment on column report_generator.started_at is '시작 시간';
 comment on column report_generator.ended_at is '종료 시간';
 comment on column report_generator.status is 'report_generator agent의 상태 - `pending`, `running`, `completed`, `failed`';
 comment on column report_generator.response is '반환값 - 성공 응답값 || 실패 에러값';
+
+insert into users (name, auth_token)
+values
+    ('user01', 'token01'),
+    ('user02', 'token02'),
+    ('user03', 'token03'),
+    ('user04', 'token04'),
+    ('user05', 'token05');
