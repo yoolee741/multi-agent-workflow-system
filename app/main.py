45260f4bcefe04de6641244a19da34efe982ab57
Yoo Rim Lee <yoolee741@gmail.com>
2025-08-12 15:31:44 +0900

    [REFACTOR] main.py, database.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, Query, WebSocket
@@ -19,8 +20,14 @@ logging.basicConfig(
 )
 
 
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    **  @app.on_event("startup") -> deprecated
+    * 애플리케이션 시작 전, 후에 실행되어야 하는 로직(코드)을 정의
+    * 전체 애플리케이션에서 사용해야 하는 자원을 설정하거나 요청 간에 공유되는 자원을 설정하고, 또는 그 후에 정리하는 데 유용
+
+    """
     retries = 10
     delay = 3
     for i in range(retries):
@@ -28,12 +35,25 @@ async def startup_event():
             pool = await connect_db()
             async with pool.acquire() as conn:
                 await conn.execute("SELECT 1")
-            print("DB 연결 성공")
-            return
+            app.state.db_pool = pool  # FastAPI 애플리케이션 인스턴스(app)의 상태 저장 공간에 커넥션 풀 객체를 저장 -> 앱이 살아있는 동안 커넥션 풀을 안전하게 공유
+            logging.info("DB 연결 성공")
+            break
         except Exception as e:
-            print(f"DB 연결 실패 {i+1}/{retries}, 재시도 중... {e}")
+            logging.warning(f"DB 연결 실패 {i + 1}/{retries}, 재시도 중... {e}")
             await asyncio.sleep(delay)
-    raise RuntimeError("DB 연결 실패 - 서버 시작 중단")
+    else:
+        logging.error("DB 연결 실패 - 서버 시작 중단")
+        raise RuntimeError("DB 연결 실패 - 서버 시작 중단")
+
+    yield  # 서버 실행
+
+    # 서버 종료 시 실행되는 코드
+    logging.info("서버 종료: 커넥션 풀 닫는 중...")
+    await app.state.db_pool.close()
+    logging.info("커넥션 풀 닫힘 완료")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class WorkflowRequest(BaseModel):
